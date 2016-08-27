1f08d73e3de08c9942b72d2db0262b4268b2f614
thayer11 <holly.coddington@gmail.com>
2016-08-27 10:54:41 -0600

    missed comma

---
+++ missed_connections/api/models/index.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+var dburl = 'mongodb://localhost:27017/missedconnections';
+
+mongoose.connect(dburl);
+
---
+++ missed_connections/api/models/message.model.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+	username : {
+		type : String,
+		required : true
+	},
+	first_name : String,
+	last_name : String,
+	city : String,
+	state : String,
+	description : String 
+});
+
+var replySchema = new mongoose.Schema({
+	response : String,
+	responder : [userSchema],
+});
+
+var messageSchema = new mongoose.Schema({ 
+	content : String 
+	location : {
+		name : String,
+		address : String
+	}
+	user : [userSchema],
+	reply : [replySchema]
+});
+
+mongoose.model('Message', messageSchema);
