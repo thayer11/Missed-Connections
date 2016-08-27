e5425de570ce14a9a2252e7e7d7c63c2909b02b3
thayer11 <holly.coddington@gmail.com>
2016-08-27 10:38:00 -0600

    seed stuff

--- missed_connections/api/data/message.model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var messageSchema = new mongoose.Schema({ 
-	user_id : String,//embed responses
-	location : String,
-	content : String 
-});
-
-mongoose.model('Message', messageSchema);
--- missed_connections/api/data/response.model.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var responseSchema = new mongoose.Schema({
-	responder_id : //figure out how to call on user_id
-	content : String 
-});
-
-mongoose.model('Response', responceSchema);
